ac8b9af8c491ca56f573dff30f14477eb6e89018
Stephen <spatterson8@wisc.edu>
2017-11-12 14:48:01 -0500

    park_viewer - Added select value to popup of user point.

--- js/user.js
+++ js/user.js
@@ -1,15 +1,30 @@
 $('#add_tree').click(function() {
-    L.DomUtil.addClass(map._container,'crosshair-cursor-enabled');
-    closeNav();
-    // toggle attribute to control map clicking
-    editing = true;
-    console.log('editing:', editing);
+    var selected_species = $('#species').val();
+    console.log("species: ", selected_species);
 
-    map.on('click', function(location) {
-        if (editing) {
-            var tree_location = new L.marker(location.latlng).addTo(map);
-            editing = false;
-            L.DomUtil.removeClass(map._container,'crosshair-cursor-enabled');
-        }
-    });
+    if (($('#species').val() === undefined) || ($('#species').val() === null)) {
+        alert('Choose a tree species!');
+    } else {
+        L.DomUtil.addClass(map._container,'crosshair-cursor-enabled');
+        closeNav();
+
+        // toggle attribute to control map clicking
+        editing = true;
+        console.log('editing:', editing);
+
+        map.on('click', function(location) {
+            if (editing) {
+                var tree_location = new L.marker(location.latlng, {
+                    draggable: true
+                });
+                tree_location.bindPopup($('#species').val());
+                tree_location.addTo(map);
+
+                editing = false;
+                L.DomUtil.removeClass(map._container,'crosshair-cursor-enabled');
+            }
+        });
+    }
+
+
 });
