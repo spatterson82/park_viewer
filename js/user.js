ac1480d6fc2f37b765864bf5a272c82cd18e832c
Stephen <spatterson8@wisc.edu>
2017-11-12 20:28:12 -0500

    park_viewer - implemented single point removal.

--- js/user.js
+++ js/user.js
@@ -16,17 +16,25 @@ $('#add_tree').click(function() {
         map.on('click', function(location) {
             if (editing) {
                 tree_location = new L.marker(location.latlng, {
-                    draggable: true
+                    draggable: true,
+                    id: 'tree_location'
                 });
                 var popup = 'Species: ' + $('#species').val() + '<br><br><a id="popup_button">Remove</a>';
                 tree_location.bindPopup(popup);
                 tree_location.addTo(map);
+
+                tree_location.on('popupopen', remove_user_point);
 
                 editing = false;
                 L.DomUtil.removeClass(map._container,'crosshair-cursor-enabled');
             }
         });
     }
-
-
 });
+
+function remove_user_point() {
+    var marker = this;
+    $("#popup_button:visible").click(function () {
+        map.removeLayer(marker);
+    });
+}
