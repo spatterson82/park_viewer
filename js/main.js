55ea71cc117d7872b061656444430584534116c2
Stephen <spatterson8@wisc.edu>
2017-11-16 22:54:09 -0500

    park_viewer - Added select box onchange event for filtering layers.

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function closeNav() {
     function build_query_combo(final_set, id) {
         // console.log("final set: ", final_set);
         var combo_html = '<select id="' + id + '_combo">';
-        var middle = '';
+        var middle = '<option value="" disabled selected style="display: none;">Filter Layer:</option>';
         var final_array = Array.from(final_set);
 
         for (var i in final_array) {
@@ -68,10 +68,10 @@ function closeNav() {
             middle += '<option value="' + final_array[i] + '">' + final_array[i] + "</option>";
         }
         combo_html += middle + "</select>";
-
-        // TODO Add styling to move select box over or underneath
         $(combo_html).appendTo("#" + id + '_div');
     }
+
+
     function get_features(features, id) {
         // console.log("new_layers: ", features);
         var query_set = new Set();
@@ -82,30 +82,73 @@ function closeNav() {
     }
 
 
+    function load_data(self) {
+        $.getJSON(base_url + get_sql_query(self.id), function(data) {
+            // TODO Create dropdown options for querying
+            get_features(data.features, self.id);
+
+            // register select box onchange
+            var combo_id = '#' + self.id + '_combo';
+            $(combo_id).change(function() {
+                filter_data(this)
+            });
+
+            var new_layers;
+            if (data.features[0].geometry.type === 'Point') {
+                new_layers = L.geoJSON(data, {
+                    onEachFeature: function (row, layer) {
+                        // console.log(row, layer);
+                        layer.bindPopup(createPopup(row));
+                    },
+                    id: self.id,
+                    pointToLayer: function (feature, latlng) {
+                        var geojsonMarkerOptions = {
+                            radius: 8,
+                            fillColor: "#ff7800",
+                            color: "#000",
+                            weight: 1,
+                            opacity: 1,
+                            fillOpacity: 0.8
+                        };
+                        return L.circleMarker(latlng, geojsonMarkerOptions);
+                    }
+                });
+            } else if (data.features[0].geometry.type === 'Polygon') {
+                new_layers = L.geoJSON(data, {
+                    onEachFeature: function (row, layer) {
+                        // console.log(row, layer);
+                        layer.bindPopup(createPopup(row));
+                    },
+                    id: self.id,
+                    style: function (feature) {
+                        if (feature.geometry.type === 'Point') {
+
+                        } else if (feature.geometry.type === 'Polygon') {
+                            switch (feature.properties.party) {
+                                case 'Republican':
+                                    return {color: "#ff0000"};
+                                case 'Democrat':
+                                    return {color: "#0000ff"};
+                            }
+                        }
+                    }
+                });
+            }
+            console.log(self.id);
+            new_layers.addTo(map);
+        });
+    }
+
+
     // checkbox logic
     var base_url = 'https://spatterson8.carto.com/api/v2/sql?format=GeoJSON&q=';
     $("input:checkbox").change(function() {
         console.log('ID: ' + this.id);
+
         if ($(this).is(":checked")) {
             console.log($('#' + this.id).is(':checked'));
             var self = this;
-            $.getJSON(base_url + get_sql_query(this.id), function(data) {
-                // TODO Create dropdown options for querying
-                get_features(data.features, self.id);
-
-                var new_layers = L.geoJSON(data, {
-                    onEachFeature: function (row, layer) {
-                        console.log(row, layer);
-                        layer.bindPopup(createPopup(row));
-                    },
-                    id: self.id
-                });
-                console.log(self.id);
-
-
-                // console.log("new_layers: ", new_layers);
-                new_layers.addTo(map);
-            });
+            load_data(self);
         } else {
             console.log($('#' + this.id).is(':checked'));
             var layer_list = map._layers;
@@ -120,6 +163,65 @@ function closeNav() {
 
         }
     });
+
+
+
+    // filtering if select box queried
+    function filter_data(select_box) {
+        console.log('select:', select_box, select_box.value);
+        // TODO make this function be a filter version of load_data()
+
+        // $.getJSON(base_url + get_sql_query(this.id), function(data) {
+        //     // TODO Create dropdown options for querying
+        //     get_features(data.features, self.id);
+        //
+        //     console.log("data:", data);
+        //     var new_layers;
+        //     if (data.features[0].geometry.type === 'Point') {
+        //         new_layers = L.geoJSON(data, {
+        //             onEachFeature: function (row, layer) {
+        //                 // console.log(row, layer);
+        //                 layer.bindPopup(createPopup(row));
+        //             },
+        //             id: self.id,
+        //             pointToLayer: function (feature, latlng) {
+        //                 var geojsonMarkerOptions = {
+        //                     radius: 8,
+        //                     fillColor: "#ff7800",
+        //                     color: "#000",
+        //                     weight: 1,
+        //                     opacity: 1,
+        //                     fillOpacity: 0.8
+        //                 };
+        //                 return L.circleMarker(latlng, geojsonMarkerOptions);
+        //             }
+        //         });
+        //     } else if (data.features[0].geometry.type === 'Polygon') {
+        //         var new_layers = L.geoJSON(data, {
+        //             onEachFeature: function (row, layer) {
+        //                 // console.log(row, layer);
+        //                 layer.bindPopup(createPopup(row));
+        //             },
+        //             id: self.id,
+        //             style: function (feature) {
+        //                 console.log("feature:", feature);
+        //                 if (feature.geometry.type === 'Point') {
+        //
+        //                 } else if (feature.geometry.type === 'Polygon') {
+        //                     switch (feature.properties.party) {
+        //                         case 'Republican':
+        //                             return {color: "#ff0000"};
+        //                         case 'Democrat':
+        //                             return {color: "#0000ff"};
+        //                     }
+        //                 }
+        //             }
+        //         });
+        //     }
+        //     console.log(self.id);
+        //     new_layers.addTo(map);
+        // });
+    }
 
 
     $(document).ready(initialize());
--- js/user.js
+++ js/user.js
@@ -14,9 +14,8 @@ $('#add_tree').click(function() {
 
         var treeicon = L.icon({
             iconUrl: 'img/tree.png',
-
             iconSize:     [38, 50], // size of the icon
-            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            iconAnchor:   [22, 40], // point of the icon which will correspond to marker's location
             popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
         });
 
