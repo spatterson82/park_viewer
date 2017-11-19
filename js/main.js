9daed7686a4fd4d6e3ae30f5a5057463a43a19d3
Stephen <spatterson8@wisc.edu>
2017-11-18 20:25:57 -0500

    park_viewer - Implemented point filtering and added docstrings.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/**
+ * Function to reopen the layer table of contents
+ */
 function openNav() {
     console.log("clicked open");
     editing = false;
@@ -8,6 +11,10 @@ function openNav() {
     $("#map_title").html('');
 }
 
+
+/**
+ * Function to hide the layer table of contents
+ */
 function closeNav() {
     console.log("clicked close");
     document.getElementById("panel_border").style.width = "0";
@@ -21,6 +28,10 @@ function closeNav() {
 (function() {
     'use strict';
 
+
+    /**
+     * Main function to instantiate a map object
+     */
     function initialize() {
         map = L.map('map', {
             center: [43.738, -69.831],
@@ -42,12 +53,23 @@ function closeNav() {
     // ##################################################
     var api_key = '0bc661c1ea32153881fc4a135ca4ccb8c18bae18';
 
-    // function to authenticate
+
+    /**
+     * Function for generating an SQL statement
+     * that selects all features for a layer
+     * @param layer CartoDB layer name
+     * @returns {string} SQL Query statement
+     */
     function get_sql_query(layer) {
         return 'SELECT * FROM ' + layer + '&api_key=' + api_key;
     }
 
 
+    /**
+     * Function to create a simple popup
+     * @param feature Current feature
+     * @returns {L.popup} Popup object for the feature
+     */
     function createPopup(feature) {
         // TODO make popup unique by layer and show everything
 
@@ -56,7 +78,12 @@ function closeNav() {
     }
 
 
-
+    /**
+     * Function to build the HTML data for the current
+     * layer's select box
+     * @param final_set Set of unique features to populate the select box
+     * @param id Element ID for the current layer
+     */
     function build_query_combo(final_set, id) {
         // console.log("final set: ", final_set);
         var combo_html = '<select id="' + id + '_combo">';
@@ -72,6 +99,12 @@ function closeNav() {
     }
 
 
+    /**
+     * Function to build the select box for the
+     * currently checked layer
+     * @param features All current features for the selected layer
+     * @param id Element ID for the selected layer
+     */
     function get_features(features, id) {
         // console.log("new_layers: ", features);
         var query_set = new Set();
@@ -82,6 +115,11 @@ function closeNav() {
     }
 
 
+    /**
+     * Main function to load data if a layer's
+     * checkbox is checked
+     * @param self Current checkbox object to pass to AJAX
+     */
     function load_data(self) {
         $.getJSON(base_url + get_sql_query(self.id), function(data) {
             // TODO Create dropdown options for querying
@@ -140,7 +178,9 @@ function closeNav() {
     }
 
 
-    // checkbox logic
+    /**
+     * Layer checkbox events and logic
+     */
     var base_url = 'https://spatterson8.carto.com/api/v2/sql?format=GeoJSON&q=';
     $("input:checkbox").change(function() {
         console.log('ID: ' + this.id);
@@ -159,8 +199,13 @@ function closeNav() {
         }
     });
 
+
     function remove_data(id) {
-        console.log('remove data:', id);
+        /**
+         * Remove the layer associated with the
+         * checkbox that was unchecked
+         */
+
         var layer_list = map._layers;
         for (var layer in layer_list) {
             if (layer_list[layer].options.id === id) {
@@ -170,9 +215,14 @@ function closeNav() {
     }
 
 
-    // filtering if select box queried
+    /**
+     * Same as load_data() but filters based on
+     * the select box value for the layer checked
+     * @param select_box Current layer's select box object
+     * @param id Element ID for the current layer that is checked
+     */
     function filter_data(select_box, id) {
-        // remove all points
+        // remove all current data for the selected layer
         remove_data(id);
 
         $.getJSON(base_url + get_sql_query(id), function(data) {
